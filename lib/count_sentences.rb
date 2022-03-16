250e66f40faa9927658d13a0d859254414473df6
Michael Dally <mddally@gmail.com>
2022-03-16 09:59:39 -0400

    commit tree cdc16cfc4936448dbe6fd6dd4de46362394ec36b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    !!self.match(/\.$/)
   end
 
   def question?
-
+    !!self.match(/\?$/)
   end
 
   def exclamation?
-
+    !!self.match(/\!$/)
   end
 
   def count_sentences
-
+    self.split(/[\.\?\!]+/).length
   end
 end
+
+puts "This is a string! It has three sentences. Right?".count_sentences
+pp "This, well, is a sentence. This is too!! And so is this, I think? Woo...".split(/[\.\?\!]+/)
